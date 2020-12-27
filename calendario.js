4663a298926a2b5b4d9302665cedeeec1c37fc61
AlanOsta <alanosta@gmail.com>
2020-12-27 17:57:31 -0300

    100% en FB-dev

--- calendario.js
+++ calendario.js
@@ -5,6 +5,117 @@ const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Juli
 const dayNames = ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"];
 const catNode = document.getElementById('categorias');
 const selCatNode = document.getElementById('selCat');
+const db = firebase.firestore();
+let numDia = 0;
+let catSeleccionada = " ";
+let anioFB = [];
+
+const cargaAnio = () => {
+    var docRef = db.collection("calendario").doc("anio");
+    docRef.get().then(function(doc) {
+            anioFB = doc.data().anioFB;						// Fetch del array completo
+            categorias = [...new Set(anioFB)];			    // Filtra solo valores unicos de categorias incluida " "
+            categorias.splice(categorias.indexOf(" "),1);	// Saco el " " del array
+            dibujaCategorias();
+    }).catch(function(error) {
+        console.log("Error al obtener el documento:", error);
+    });
+};
+
+const salvaAnio = (anioFB) => db.collection("calendario").doc("anio").set({anioFB});
+
+function dibujaCategorias() {
+    while (selCat.firstChild) {selCat.removeChild(selCat.firstChild)};
+    let tituloCategorias = document.createElement("h4");
+    tituloCategorias.innerText = "Seleccione una categoria: ";
+    tituloCategorias.classList.add ("titulo");
+    selCatNode.appendChild(tituloCategorias);
+    for (let i = 0; i < categorias.length ; i++){
+        if (categorias[i] != " "){
+            let buttonCat = document.createElement('button');
+            buttonCat.classList.add("buttonCat");
+            buttonCat.type = "submit";
+            buttonCat.name = categorias[i];
+            buttonCat.innerText = categorias[i];
+            selCatNode.appendChild(buttonCat);
+        };
+    };
+    catNode.addEventListener("click", catClickHandler);						
+};
+
+// Categorias ClickHandler
+function catClickHandler(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    let nameCat = e.target.getAttribute("name");
+    let buttonNodes = document.getElementsByClassName("buttonCat");
+    let inputCatValue = document.getElementById("addCatValue").value
+    let calendario = document.getElementById("calendario");
+    let daysColl = calendario.getElementsByClassName("day"); // Colleccion de todos los dias
+
+    if (nameCat == "addCat"){
+        if (nameCat == " "){
+            alert("La categoria no puede empezar por un espacio");
+        }else {
+            categorias.push(inputCatValue);
+            console.log(categorias);
+            dibujaCategorias();
+        };
+    };
+
+    if (nameCat == "delCat"){
+        categorias.splice(categorias.indexOf(inputCatValue),1);	// Borro la categoria del array de categorias
+        dibujaCategorias();
+        for (let h = 0; h < daysColl.length; h++){
+            let dayCatValue = daysColl[h].getAttribute("categoria");
+
+            if (dayCatValue == inputCatValue) {
+                daysColl[h].removeAttribute("categoria");
+                daysColl[h].setAttribute("categoria"," ");
+                daysColl[h].classList.remove("seleccionado");
+                anioFB[h] = " ";
+                console.log(anioFB);
+            }						
+        }
+        salvaAnio(anioFB);
+    };
+
+        // Manejo de clase "seleccionado" para los botones de categorias
+        for (i = 0; i < categorias.length; i++){
+            let buttonOff = buttonNodes[i];
+
+            if (e.target.id == "categorias" ) {return;};
+
+            if (nameCat == categorias[i]) {
+                e.target.classList.add('seleccionado');
+                catSeleccionada = categorias[i];
+                inputCatRef = document.getElementById("addCatValue");						
+                inputCatRef.value = nameCat;
+            } else {
+                    buttonOff.classList.remove('seleccionado');
+                };
+        };
+
+        // Reasigna clase "seleccionado" de acuerdo a la categoria seleccionada
+        for (let j = 0; j < daysColl.length; j++){
+            if (anioFB[j] == catSeleccionada){		// Compara el array con la categoria seleccionada
+                daysColl[j].classList.add("seleccionado");
+                daysColl[j].setAttribute("categoria",anioFB[j]);
+            }else {
+                daysColl[j].classList.remove("seleccionado");
+                daysColl[j].setAttribute("categoria",anioFB[j]);
+            };
+        };
+};
+
+// Categorias Array Handler
+function catArrayHandler(dia, categoria) {
+    if (anioFB[dia] == categoria){
+        anioFB[dia] = " ";
+    }else {
+        anioFB[dia] = categoria;
+    }
+};
 
 function getDaysInMonth(mes, anio) {
     let fecha = new Date(anio, mes, 1);
@@ -14,7 +125,7 @@ function getDaysInMonth(mes, anio) {
         fecha.setDate(fecha.getDate() + 1);
     }
     return dias;
-}
+};
 
 function getMonthsInYear(anio) {
     let fecha = new Date(anio, 0, 1);
@@ -28,19 +139,14 @@ function getMonthsInYear(anio) {
     return meses;
 }
 
-// Create a full 12-month calendar
+// Crea el calendario completo de 12 meses
 function buildYearCalendar(ref, anio) {
-    // cargaAnio();
+    cargaAnio();
     let meses = getMonthsInYear(anio);
-/////////////////////////////////        opts   /////////////////////////////////////////////////
     let opts = {
-        showMonth: true,
-        showDaysOfWeek: true,
-        showYear: true,
-/////////////////////////////////     Dia   ClickHandler   /////////////////////////////////////////////////
         clickHandler: function(e) {
             
-         /*   let dayNum = e.target.getAttribute("numero");
+            let dayNum = e.target.getAttribute("numero");
             let catDay = e.target.getAttribute("categoria")
 
             // Si hay una categoria seleccionada
@@ -52,35 +158,32 @@ function buildYearCalendar(ref, anio) {
                     // Borra la categoria
                     e.target.classList.remove("seleccionado");
                     e.target.setAttribute("categoria", " ");
-                    arrayHandler(dayNum, " ");
+                    catArrayHandler(dayNum, " ");
                 }else {
                     e.target.classList.add('seleccionado');
                     e.target.setAttribute("categoria", catSeleccionada);
-                    arrayHandler(dayNum, catSeleccionada);
+                    catArrayHandler(dayNum, catSeleccionada);
                     console.dir(dayNum);
                 }
             
             } else {alert("no hay categoria seleccionada")};
-            salvaAnio(anio);*/
+            salvaAnio(anioFB);
         }
-    }
+    };
 
-    // dibujaCategorias();
+    dibujaCategorias();
 
     meses.forEach(function(a, b) {
         var $monthNode = buildMonth(b, anio, opts);
         ref.appendChild($monthNode);
     });
+};
 
 function buildMonth(monthNum, year, opts) {
-        //if (monthNum === undefined || year === undefined) return "something is missing";
-        let _this = this;
-        let dtm = new Date(year, monthNum, 1);
-        let dtmMonth = dtm.getMonth();
-        let daysInMonth = _this.getDaysInMonth(monthNum, year);  
+        let daysInMonth = getDaysInMonth(monthNum, year);  
         let $monthNode = document.createElement('div');
         let $titleNode = document.createElement('h4');
-        let skipLength = daysInMonth[0].getDay();
+        let skipLength = daysInMonth[0].getDay();   // Devuelve la cantidad de dias de la semana que se deven saltear al comienzo del mes
         let preLength = daysInMonth.length + skipLength;
         let postLength = function() {
             if (preLength % 7 === 0) {
@@ -96,61 +199,42 @@ function buildMonth(monthNum, year, opts) {
 
         $monthNode.classList.add('month');
 
-        // Add a Title to the month
-        if (opts.showMonth) {
-            $titleNode.innerText = monthNames[monthNum] + (opts.showYear ? " " + year : '');
-            $monthNode.appendChild($titleNode);
-        }
-        // Add Days of week to the top row
-        if (opts.showDaysOfWeek) {
-            dayNames.forEach(function(a, b) {
-                let $dayNode = document.createElement('div');
-                $dayNode.classList.add('dow');
-                $dayNode.innerText = dayNames[b];
-                $monthNode.appendChild($dayNode);
-            });
-        }
-        // Add blank days to fill in before first day
+        // Agrega el titulo al mes (Nombre + Año)
+        $titleNode.innerText = monthNames[monthNum] + (" " + year);
+        $monthNode.appendChild($titleNode);
+        
+        // Agrega los nombres de dias de la semana a la primera fila
+        dayNames.forEach(function(a, b) {
+            let $dayNode = document.createElement('div');
+            $dayNode.classList.add('dow');
+            $dayNode.innerText = dayNames[b];
+            $monthNode.appendChild($dayNode);
+        });
+        
+        // Agrega los dias en blanco antes del primer dia del mes
         for (let i = 0; i < skipLength; i++) {
             let $dayNode = document.createElement('div');
             $dayNode.classList.add('dummy-day');
             $monthNode.appendChild($dayNode);
         }
-//////////////////////////// Dibuja cada dia del Mes ////////////////////////////
+
         // Place a day for each day of the month
         daysInMonth.forEach(function(c, d) {
-            let today = new Date(new Date().setHours(0, 0, 0, 0));
             let $dayNode = document.createElement('div');
             $dayNode.classList.add('day');
-        //    $dayNode.setAttribute("numero", numDia);
+            $dayNode.setAttribute("numero", numDia);
 
-        //	$dayNode.setAttribute("categoria", anio[numDia]);
-        //    numDia++;
+        	$dayNode.setAttribute("categoria", anioFB[numDia]);
+            numDia++;
 
-            //anio.push(" ");
+            // Agregar condicion de inicializacion del array del año
+            //anioFB.push(" ");
+            
             
             $dayNode.innerText = (d + 1);
             let dow = new Date(c).getDay();
 
             if (dow === 0 || dow === 6) $dayNode.classList.add('weekend');
-            if (opts.onlyCurrent && c < today) $dayNode.classList.add('dummy-day');
-            if (opts.limitDate) {
-                if (c > opts.limitDate) {
-                    $dayNode.classList.add('dummy-day');
-                }
-            }
-
-            if (opts.filterDayOfWeek) {
-                let valid = false;
-                for (let i = 0; i < opts.filterDayOfWeek.length; i++) {
-                    if (c.getDay() == opts.filterDayOfWeek[i]) {
-                        valid = true;
-                    }
-                }
-                if (!valid) {
-                    $dayNode.classList.add('dummy-day');
-                }
-            }
             if (opts.clickHandler && !$dayNode.classList.contains('dummy-day')) {
                 function handleEvent(e) {
                     e = e || window.event;
@@ -179,17 +263,11 @@ function buildMonth(monthNum, year, opts) {
             $dayNode.innerText = j + 1;
             $monthNode.appendChild($dayNode);
         }
-
         return $monthNode;
-
-    }
 }
 
-//console.log(getDaysInMonth(0, 2021));
-//console.log(getMonthsInYear(2021));
 
-var calendarioRef = document.getElementById("calendario");
-var currentYear = new Date().getFullYear();
-//var calendario = new crearCalendario();
+let calendarioRef = document.getElementById("calendario");
+let currentYear = new Date().getFullYear();
 buildYearCalendar(calendarioRef, currentYear);
 
